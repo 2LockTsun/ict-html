cbe1c317e18c3d91ffa47c4d1af780ce41bcbc22
2LockTsun <wonkalok5@outlook.com>
2022-06-01 10:47:54 +0800

    unknown commit

--- src/next-app/pages/support/index.js
+++ src/next-app/pages/support/index.js
@@ -5,7 +5,15 @@ export default function SupportPage() {
     return (
         <div>
             <NavigationBar  />
-            
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/YgOi41qmlc4"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            />
             <footer className={styles.footer}>
                 Copyrights belongs to :
                 <span ><a href='https://github.com/2LockTsun/ict-html'>2_Lock_Tsun</a></span>
