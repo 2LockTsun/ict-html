0a1a38727d8847a468893a7204e044bc0a076989
2LockTsun <wonkalok5@outlook.com>
2022-06-05 22:09:30 +0800

    small changes in some file, I dunno what is that

--- src/next-app/pages/championship/seasons.js
+++ src/next-app/pages/championship/seasons.js
@@ -8,83 +8,99 @@ import { TableHead, TableRow, TableCell } from '@mui/material';
 import Link from 'next/link';
 
 export default function Seasons() {
-    const [ Season, setSeason ] = useState(0)
-    const [ Type, setType ] = useState(0)
+	const [Season, setSeason] = useState(0);
+	const [Type, setType] = useState(0);
 
-    const table = seasons.map(season => {return([
-        <TableHead>
-            <TableRow>
-                <TableCell>Circuit</TableCell>
-                <TableCell>Driver</TableCell>
-                <TableCell>Team</TableCell>
-                <TableCell>Laps</TableCell>
-                <TableCell>Time</TableCell>
-            </TableRow>
-            {season.races.map(race => {return(
-                <TableRow>
-                    <TableCell>{race.circuit}</TableCell>
-                    <TableCell>{race.driver}</TableCell>
-                    <TableCell>{race.team}</TableCell>
-                    <TableCell>{race.laps}</TableCell>
-                    <TableCell>{race.time}</TableCell>
-                </TableRow>
-            )})}
-        </TableHead>,
-        <TableHead>
-            <TableRow>
-                <TableCell>Position</TableCell>
-                <TableCell>Driver</TableCell>
-                <TableCell>Team</TableCell>
-                <TableCell>Points</TableCell>
-            </TableRow>
-            {season.drivers.map(driver => {return(
-                <TableRow>
-                    <TableCell>{driver.post}</TableCell>
-                    <TableCell>{driver.driver}</TableCell>
-                    <TableCell>{driver.team}</TableCell>
-                    <TableCell>{driver.points}</TableCell>
-                </TableRow>
-            )})}
-        </TableHead>,
-        <div><TableHead>
-            <TableRow>
-                <TableCell>Position</TableCell>
-                <TableCell>Team</TableCell>
-                <TableCell>Points</TableCell>
-            </TableRow>
-            {season.teams.map(team => {return(
-                <TableRow>
-                    <TableCell>{team.post}</TableCell>
-                    <TableCell>{team.team}</TableCell>
-                    <TableCell>{team.points}</TableCell>
-                </TableRow>
-            )})}
-        </TableHead></div>
-    ])})
+	const table = seasons.map((season) => {
+		return [
+			<TableHead>
+				<TableRow>
+					<TableCell>Circuit</TableCell>
+					<TableCell>Driver</TableCell>
+					<TableCell>Team</TableCell>
+					<TableCell>Laps</TableCell>
+					<TableCell>Time</TableCell>
+				</TableRow>
+				{season.races.map((race) => {
+					return (
+						<TableRow>
+							<TableCell>{race.circuit}</TableCell>
+							<TableCell>{race.driver}</TableCell>
+							<TableCell>{race.team}</TableCell>
+							<TableCell>{race.laps}</TableCell>
+							<TableCell>{race.time}</TableCell>
+						</TableRow>
+					);
+				})}
+			</TableHead>,
+			<TableHead>
+				<TableRow>
+					<TableCell>Position</TableCell>
+					<TableCell>Driver</TableCell>
+					<TableCell>Team</TableCell>
+					<TableCell>Points</TableCell>
+				</TableRow>
+				{season.drivers.map((driver) => {
+					return (
+						<TableRow>
+							<TableCell>{driver.post}</TableCell>
+							<TableCell>{driver.driver}</TableCell>
+							<TableCell>{driver.team}</TableCell>
+							<TableCell>{driver.points}</TableCell>
+						</TableRow>
+					);
+				})}
+			</TableHead>,
+			<TableHead>
+				<TableRow>
+					<TableCell>Position</TableCell>
+					<TableCell>Team</TableCell>
+					<TableCell>Points</TableCell>
+				</TableRow>
+				{season.teams.map((team) => {
+					return (
+						<TableRow>
+							<TableCell>{team.post}</TableCell>
+							<TableCell>{team.team}</TableCell>
+							<TableCell>{team.points}</TableCell>
+						</TableRow>
+					);
+				})}
+			</TableHead>,
+		];
+	});
 
 	return (
 		<div>
 			<Head>
-                <title>Seasons - My favourite sports</title>
-			    <meta name='description' content='A website for ICT homework' />
-			    <link rel='icon' href='/favicon.ico' />
-            </Head>
+				<title>Seasons - My favourite sports</title>
+				<meta name='description' content='A website for ICT homework' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			<NavigationBar />
 			<div className={styles.container}>
-                <div className={styles.Nav}>
-                    <Link href={''}><a onClick={() => setSeason(0)}>2021</a></Link>
-                    <Link href={''}><a onClick={() => setSeason(1)}>2022</a></Link>
-                </div>
-                <div className={styles.Nav}>
-                    <Link href={''}><a onClick={() => setType(0)}>Races</a></Link>
-                    <Link href={''}><a onClick={() => setType(1)}>Drivers</a></Link>
-                    <Link href={''}><a onClick={() => setType(2)}>Drivers</a></Link>
-                </div>
-                <div className={styles.table}>
-                    {table[Season][Type]}
-                </div>
-            </div>
+				<div className={styles.Nav}>
+					<Link href={''}>
+						<a onClick={() => setSeason(0)}>2021</a>
+					</Link>
+					<Link href={''}>
+						<a onClick={() => setSeason(1)}>2022</a>
+					</Link>
+				</div>
+				<div className={styles.Nav}>
+					<Link href={''}>
+						<a onClick={() => setType(0)}>Races</a>
+					</Link>
+					<Link href={''}>
+						<a onClick={() => setType(1)}>Drivers</a>
+					</Link>
+					<Link href={''}>
+						<a onClick={() => setType(2)}>Drivers</a>
+					</Link>
+				</div>
+				<div className={styles.table}>{table[Season][Type]}</div>
+			</div>
 			<Footer />
 		</div>
-	)
+	);
 }
