8081000f9be9c94e281d3553e8bfe18402d9f687
2LockTsun <wonkalok5@outlook.com>
2022-06-05 13:55:48 +0800

    Did I provide support method to this site

--- src/next-app/pages/support/contactUs.js
+++ src/next-app/pages/support/contactUs.js
@@ -1,7 +1,11 @@
+import styles from "../../styles/pages/Support.module.css"
+
 export default function ContactUsPage() {
     return(
         <div>
             <h1>You will never able to contact us</h1>
+            <div className={styles.whitespace} />
+            <p className={styles.dc}>Congrats! You are able to find me on Discord now.<br />Discord : WKL26004#7714</p>
         </div>
     )
 }
--- src/next-app/pages/support/siteMap.js
+++ src/next-app/pages/support/siteMap.js
@@ -1,4 +1,5 @@
 import styles from "../../styles/pages/Support.module.css";
+import Link from "next/link";
 
 export default function SiteMapPage() {
     return(
@@ -6,8 +7,27 @@ export default function SiteMapPage() {
             <div className={styles.title}>
                 <h1>Site Map</h1>
             </div>
-            <div>
-                
+            <div className={styles.content}>
+                <Link href={'/'}><a>Home</a></Link>
+                <div className={styles.blank} />
+                <Link href={'/about'}><a>About</a></Link>
+                    <li>Formula One</li>
+                    <li>Developer</li>
+                <div className={styles.blank} />
+                <Link href={'/championship'}><a>Championship</a></Link>
+                    <li>Teams</li>
+                    <li>Drivers</li>
+                    <li>Tracks</li>
+                <div className={styles.blank} />
+                <Link href={'/game'}><a>Game</a></Link>
+                    <li>Codemaster</li>
+                    <li>F1 22</li>
+                    <li>F1 2021</li>
+                    <li>F1 2020</li>
+                <div className={styles.blank} />
+                <Link href={'/support'}><a>Support</a></Link>
+                    <li>Contact Us</li>
+                    <li>Site Map</li>
             </div>
         </div>
     )
